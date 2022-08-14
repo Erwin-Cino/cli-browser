8427ab0bf75d5f8ccd0cfe04fc155d291e13c68e
Erwin <erwincino0812@gmail.com>
2022-08-14 21:46:53 +0800

    set up function for transpiling code

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,11 +1,30 @@
-import {useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('')
-    const onSubmit = () => {
-        console.log("INPUT TYPE", input)
+    const [code, setCode] = useState('');
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+        // console.log("SERVICE", service);
+    }
+    useEffect(() => {
+        startService();
+    }, []);
+    const onSubmit = async () => {
+        if (!ref.current) return;
+        console.log("INPUT TYPE", ref.current);
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+        console.log("RESULT", result)
+        setCode(result.code)  
     }
     return (
         <div>
