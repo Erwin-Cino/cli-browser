b282fc30a616f82d1493e6c8d53768a5f141b39c
Erwin <erwincino0812@gmail.com>
2022-08-16 20:30:38 +0800

    Fix nested packages directory

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -13,14 +13,14 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           }
         }
 
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
-        }
+        } 
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -30,17 +30,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const {data} = await axios.get(args.path)
+        const {data, request} = await axios.get(args.path)
+        console.log("REQUEST", request)
         console.log("DATA FETCH", data)
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
       });
     },
