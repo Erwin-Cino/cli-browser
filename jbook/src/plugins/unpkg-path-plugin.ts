6d39c6ae77acb41b6026c05eecf073bc2d8253de
Erwin <erwincino0812@gmail.com>
2022-08-17 15:14:52 +0800

    fix dynamic calling of packages and test

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -30,7 +30,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
+              import React, {useState} from 'react';
               console.log(message);
             `,
           };
