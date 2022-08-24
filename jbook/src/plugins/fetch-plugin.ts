f6c98a27ecafd4a0dc22e25beabc4500ec581f9e
Erwin <erwincino0812@gmail.com>
2022-08-24 21:25:22 +0800

    change loaders way of filtering

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -10,40 +10,83 @@ export const fetchPlugin = (inputCode: string) => {
     return {
         name: 'feth=ch-plugin',
         setup(build: esbuild.PluginBuild) {
-        build.onLoad({ filter: /.*/ }, async (args: any) => {
-            console.log('onLoad', args);
-    
-            if (args.path === 'index.js') {
-              return {
+        build.onLoad({ filter: /^index\.js$/ }, () => {
+            return {
                 loader: 'jsx',
                 contents: inputCode,
               };
-            }
-    
-            // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-            //   args.path
-            // );
-    
-            // if (cachedResult) {
-            //   return cachedResult;
-            // }
-            const { data, request } = await axios.get(args.path);
-            const fileType = args.path.endsWith('.css') ? 'css' : 'jsx';
-            const contents = fileType === 'css' ?
-             `
-                const style = document.createElement('style'); 
-                style.innerText = ${JSON.stringify(data)}; 
-                document.head.appendChild(style);
-             ` : data;
-            const result: esbuild.OnLoadResult = {
-              loader: 'jsx',
-              contents: contents,
-              resolveDir: new URL('./', request.responseURL).pathname,
-            };
-            await fileCache.setItem(args.path, result);
-    
-            return result;
-          });
-        }
+        })
+
+        build.onLoad({ filter: /.*/ }, async (args: any) => {
+          const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+            args.path
+          );
+
+          if (cachedResult) {
+            return cachedResult;
+          }
+        });
+       
+        build.onLoad({ filter: /.css$/ }, async (args: any) => {
+          
+          const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+            args.path
+          );
+  
+          if (cachedResult) {
+            return cachedResult;
+          }
+          const { data, request } = await axios.get(args.path);
+          
+          const result: esbuild.OnLoadResult = {
+            loader: 'jsx',
+            contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname,
+          };
+          await fileCache.setItem(args.path, result);
+  
+          return result;
+        });
+
+        build.onLoad({ filter: /.css$/ }, async (args: any) => {
+          const { data, request } = await axios.get(args.path);
+          const escaped = data
+            .replace(/\n/g, '')
+            .replace(/"/g, '\\"')
+            .replace(/'/g, "\\'");
+          const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `;
+  
+          const result: esbuild.OnLoadResult = {
+            loader: 'jsx',
+            contents,
+            resolveDir: new URL('./', request.responseURL).pathname,
+          };
+          await fileCache.setItem(args.path, result);
+  
+          return result;
+        });
+  
+        build.onLoad({ filter: /.*/ }, async (args: any) => {
+          const { data, request } = await axios.get(args.path);
+  
+          const result: esbuild.OnLoadResult = {
+            loader: 'jsx',
+            contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname,
+          };
+          await fileCache.setItem(args.path, result);
+  
+          return result;
+        });
+
+      }
     }
 }
+
+// * A Code IDE that can be screen shared for the live coding test; 
+// * One of your codes, based on which the Interviewer will ask questions; 
+// * One of your previous projects that can be screen shared.
