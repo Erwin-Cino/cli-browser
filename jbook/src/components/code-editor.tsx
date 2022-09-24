2274cceeb59b49b94f13a4dd8b34cd1decf67e62
Erwin <erwincino0812@gmail.com>
2022-09-24 22:37:03 +0800

    fix typescript

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,6 +1,5 @@
-import MonacoEditor from '@monaco-editor/react';
-import { SetStateAction, useState, useRef } from 'react';
-import { EditorOptions } from 'typescript';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import { useState } from 'react';
 
 interface CodeEditorProps {
     initialValue: string;
@@ -9,12 +8,12 @@ interface CodeEditorProps {
 
 const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, onChange}) => {
     const [input, setInput] = useState('');
-    console.log('input editor', input)
-    const editorRef = useRef();
-    const onEditorDidMount = (getValue: () => string, monacoEditor:any) => {
+    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue())
         })
+
+        monacoEditor.getModel()?.updateOptions({tabSize: 2});
     }
     return (
         <MonacoEditor
