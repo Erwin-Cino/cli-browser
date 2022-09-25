d708baa64ef11d97faf4dbc69459f2646fc4a2dc
Erwin <erwincino0812@gmail.com>
2022-09-26 01:59:21 +0800

    prettify code

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,41 +1,40 @@
-import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
-import { useState } from 'react';
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface CodeEditorProps {
-    initialValue: string;
-    onChange(value: string): void;
+  initialValue: string;
+  onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({initialValue, onChange}) => {
-    const [input, setInput] = useState('');
-    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-        monacoEditor.onDidChangeModelContent(() => {
-            onChange(getValue())
-        })
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const [input, setInput] = useState("");
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
 
-        monacoEditor.getModel()?.updateOptions({tabSize: 2});
-    }
-    return (
-        <MonacoEditor
-            editorDidMount={onEditorDidMount}
-            value={initialValue}
-            language='javascript' 
-            height="500px"
-            theme="vs-dark"
-            options={{
-                wordWrap: 'on',
-                minimap: { enabled: false },
-                folding: false,
-                showUnused: false,
-                lineNumbersMinChars: 3,
-                fontSize: 16,
-                scrollBeyondLastLine: false,
-                automaticLayout: true
-            }}
-            // ref={editorRef}
-        />
-    )
-    
-}
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      language="javascript"
+      height="500px"
+      theme="vs-dark"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        folding: false,
+        showUnused: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+      // ref={editorRef}
+    />
+  );
+};
 
 export default CodeEditor;
