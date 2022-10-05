9a728058b80df679c77e6f9b3ea98049ef969c41
Erwin <erwincino0812@gmail.com>
2022-10-05 22:08:29 +0800

    add css to the code editor

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -2,6 +2,7 @@ import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import { useState, useRef } from "react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -37,8 +38,13 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     editorRef.current.setValue(formatted);
   };
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
