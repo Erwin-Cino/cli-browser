5f3ac2f659a9730dfea38e663442f2aa6401c1ff
Erwin <erwincino0812@gmail.com>
2022-10-12 00:40:18 +0800

    refactor code and clean up index

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,46 +1,23 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
-
+import bundle from './bundler';
 const App = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
+
 
   const onSubmit = async () => {
-    if (!ref.current) return;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output)
   };
 
   return (
     <div>
       <CodeEditor
-        initialValue="const a = 1"
+        initialValue="console.log('Hello')"
         onChange={(value) => setInput(value)}
       />
       <div>
