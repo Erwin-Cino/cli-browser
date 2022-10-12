c5a4f7e006f5331fd6403f8ba11aa6aff4eb56f9
Erwin <erwincino0812@gmail.com>
2022-10-12 14:23:17 +0800

    set up resizable vertical component

---
+++ jbook/src/components/resizable.tsx
@@ -0,0 +1,24 @@
+import { ResizableBox } from "react-resizable";
+import './resizable.css';
+
+interface ResizableProps {
+  direction: "horizontal" | "vertical";
+  children?: React.ReactNode;
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return (
+    <ResizableBox
+      height={300}
+      width={Infinity}
+      resizeHandles={["s"]}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 100]}
+    >
+      {children}
+    </ResizableBox>
+  );
+};
+
+
+export default Resizable;
